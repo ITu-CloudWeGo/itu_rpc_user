a4ff1c2ee59eb1bf285aace3dc649b4b0061c8bd
DonCZ <1356918183@qq.com>
2025-03-31 10:04:44 +0800

    🐞fix：添加日记，统一格式化代码

--- config/config.go
+++ config/config.go
@@ -2,32 +2,44 @@ package config
 
 import (
 	_ "embed"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/goccy/go-yaml"
 	"os"
+	"strconv"
 	"sync"
 )
 
 type Config struct {
+	Kitex struct {
+		Service       string `yaml:"Service"`
+		Address       string `yaml:"Address"`
+		LogLevel      string `yaml:"LogLevel"`
+		LogFileName   string `yaml:"LogFileName"`
+		LogMaxSize    int    `yaml:"LogMaxSize"`
+		LogMaxAge     int    `yaml:"LogMaxAge"`
+		LogMaxBackups int    `yaml:"LogMaxBackups"`
+	} `yaml:"Kitex"`
+
 	PostgresSQL struct {
-		Host     string `yaml:"host"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
-		DBName   string `yaml:"dbname"`
-		Port     int    `yaml:"port"`
-		Sslmode  string `yaml:"sslmode"`
+		Host     string `yaml:"Host"`
+		User     string `yaml:"User"`
+		Password string `yaml:"Password"`
+		DBName   string `yaml:"Dbname"`
+		Port     int    `yaml:"Port"`
+		Sslmode  string `yaml:"Sslmode"`
 	} `yaml:"PostgresSQL"`
 
 	Redis struct {
-		Host     string `yaml:"host"`
-		Port     int    `yaml:"port"`
-		Password string `yaml:"password"`
-		DB       int    `yaml:"db"`
+		Address  string `yaml:"Address"`
+		Username string `yaml:"Username"`
+		Password string `yaml:"Password"`
+		DB       int    `yaml:"DB"`
 	} `yaml:"Redis"`
 
 	Registry struct {
-		RegistryAddress string `yaml:"registryAddress"`
-		UserName        string `yaml:"userName"`
-		Password        string `yaml:"password"`
+		RegistryAddress []string `yaml:"RegistryAddress"`
+		Username        string   `yaml:"Username"`
+		Password        string   `yaml:"Password"`
 	} `yaml:"Registry"`
 }
 
@@ -46,6 +58,28 @@ func GetConfig() *Config {
 	return instance
 }
 
+func LogLevel() klog.Level {
+	level := GetConfig().Kitex.LogLevel
+	switch level {
+	case "trace":
+		return klog.LevelTrace
+	case "debug":
+		return klog.LevelDebug
+	case "info":
+		return klog.LevelInfo
+	case "notice":
+		return klog.LevelNotice
+	case "warn":
+		return klog.LevelWarn
+	case "error":
+		return klog.LevelError
+	case "fatal":
+		return klog.LevelFatal
+	default:
+		return klog.LevelInfo
+	}
+}
+
 func loadConfig() *Config {
 	var conf Config
 
@@ -54,9 +88,39 @@ func loadConfig() *Config {
 		panic(err)
 	}
 
-	etcdHostEnv := os.Getenv("ETCD_HOST")
-	if etcdHostEnv != "" {
-		conf.Registry.RegistryAddress = etcdHostEnv
+	// Redis
+	redisAddrEnv := os.Getenv("REDIS_ADDR")
+	if redisAddrEnv != "" {
+		conf.Redis.Address = redisAddrEnv
+	}
+	redisUserNameEnv := os.Getenv("REDIS_USERNAME")
+	if redisUserNameEnv != "" {
+		conf.Redis.Username = redisUserNameEnv
+	}
+	redisPasswdEnv := os.Getenv("REDIS_PASSWORD")
+	if redisPasswdEnv != "" {
+		conf.Redis.Password = redisPasswdEnv
+	}
+	redisDBEnv := os.Getenv("REDIS_DB")
+	if redisDBEnv != "" {
+		db, err := strconv.Atoi(redisDBEnv)
+		if err == nil {
+			conf.Redis.DB = db
+		}
+	}
+
+	// Registry
+	registryAddrEnv := os.Getenv("REGISTRY_ADDR")
+	if registryAddrEnv != "" {
+		conf.Registry.RegistryAddress = []string{registryAddrEnv}
+	}
+	registryUserNameEnv := os.Getenv("REGISTRY_USERNAME")
+	if registryUserNameEnv != "" {
+		conf.Registry.Username = ""
+	}
+	registryPasswordEnv := os.Getenv("REGISTRY_PASSWORD")
+	if registryPasswordEnv != "" {
+		conf.Registry.Password = registryPasswordEnv
 	}
 
 	return &conf
--- main.go
+++ main.go
@@ -3,33 +3,88 @@ package main
 import (
 	"github.com/ITu-CloudWeGo/itu_rpc_user/config"
 	user_service "github.com/ITu-CloudWeGo/itu_rpc_user/kitex_gen/user_service/userservice"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
-	etcd "github.com/kitex-contrib/registry-etcd"
-	"log"
+	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
+	etcdRegistry "github.com/kitex-contrib/registry-etcd"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"net"
 	"time"
 )
 
+//func main() {
+//	conf := config.GetConfig()
+//
+//	r, err := etcd.NewEtcdRegistry(conf.Registry.RegistryAddress,
+//		etcd.WithDialTimeoutOpt(10*time.Second),
+//	)
+//	if err != nil {
+//		log.Fatalf("初始化 Etcd 注册中心失败: %v", err)
+//	}
+//
+//	svr := user_service.NewServer(
+//		new(UserServiceImpl),
+//		server.WithRegistry(r),
+//		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+//			ServiceName: "user",
+//		}),
+//	)
+//
+//	if err := svr.Run(); err != nil {
+//		log.Fatalf("服务器启动失败: %v", err)
+//	}
+//}
+
 func main() {
-	conf := config.GetConfig()
+	opts := kitexInit()
 
-	r, err := etcd.NewEtcdRegistry(
-		[]string{conf.Registry.RegistryAddress},
-		etcd.WithDialTimeoutOpt(10*time.Second),
-	)
+	svr := user_service.NewServer(new(UserServiceImpl), opts...)
+	err := svr.Run()
 	if err != nil {
-		log.Fatalf("初始化 Etcd 注册中心失败: %v", err)
+		klog.Error(err.Error())
 	}
 
-	svr := user_service.NewServer(
-		new(UserServiceImpl),
-		server.WithRegistry(r),
-		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-			ServiceName: "user",
+}
+
+func kitexInit() (opts []server.Option) {
+	conf := config.GetConfig()
+	// address
+	addr, err := net.ResolveTCPAddr("tcp", conf.Kitex.Address)
+	if err != nil {
+		panic(err)
+	}
+	opts = append(opts, server.WithServiceAddr(addr))
+
+	// service info
+	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+		ServiceName: conf.Kitex.Service,
+	}))
+	// registry_etcd
+	r, err := etcdRegistry.NewEtcdRegistry(conf.Registry.RegistryAddress)
+	if err != nil {
+		panic(err)
+	}
+	opts = append(opts, server.WithRegistry(r))
+
+	// klog
+	logger := kitexlogrus.NewLogger()
+	klog.SetLogger(logger)
+	klog.SetLevel(config.LogLevel())
+	asyncWriter := &zapcore.BufferedWriteSyncer{
+		WS: zapcore.AddSync(&lumberjack.Logger{
+			Filename:   conf.Kitex.LogFileName,
+			MaxSize:    conf.Kitex.LogMaxSize,
+			MaxBackups: conf.Kitex.LogMaxBackups,
+			MaxAge:     conf.Kitex.LogMaxAge,
 		}),
-	)
-
-	if err := svr.Run(); err != nil {
-		log.Fatalf("服务器启动失败: %v", err)
+		FlushInterval: time.Minute,
 	}
+	klog.SetOutput(asyncWriter)
+	server.RegisterShutdownHook(func() {
+		asyncWriter.Sync()
+	})
+
+	return
 }
